e025221ceafaa13b5ab26a7f1b89ba78221be7cf
5nam <ohnam90@gmail.com>
2023-02-07 16:27:12 +0900

    feat: 여러 개 마커찍는 코드 추가

--- 맛집지도/script.js
+++ 맛집지도/script.js
@@ -44,3 +44,46 @@ const dataSet = [
       category: "일식",
     },
   ];
+
+/*
+**********************************************************
+3. 여러개 마커 찍기
+  * 주소 - 좌표 변환
+https://apis.map.kakao.com/web/sample/multipleMarkerImage/ (여러개 마커)
+https://apis.map.kakao.com/web/sample/addr2coord/ (주소로 장소 표시하기)
+*/
+
+// 주소 - 좌표 변환 함수 (비동기 문제 발생 해결) ****************
+// 주소-좌표 변환 객체를 생성합니다
+var geocoder = new kakao.maps.services.Geocoder();
+function getCoordsByAddress(address) {
+  // promise 형태로 반환
+  return new Promise((resolve, reject) => {
+    // 주소로 좌표를 검색합니다
+    geocoder.addressSearch(address, function (result, status) {
+      // 정상적으로 검색이 완료됐으면
+      if (status === kakao.maps.services.Status.OK) {
+        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+        return resolve(coords);
+      }
+      reject(new Error("getCoordsByAddress Error: not valid Address"));
+    });
+  });
+}
+
+async function setMap() {
+  for (var i = 0; i < dataSet.length; i++) {
+    let position = await getCoordsByAddress(dataSet[i].address);
+    console.log(position);
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+      map: map, // 마커를 표시할 지도
+      // position: positions[i].latlng, // 마커를 표시할 위치
+      position: position,
+      // title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+    });
+  }
+}
+
+setMap();

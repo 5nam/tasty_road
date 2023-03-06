4f439797ea92cdd7a96470517ca1ae6c5588ef50
5nam <ohnam90@gmail.com>
2023-03-06 15:39:12 +0900

    style: express.js 주석 추가

--- 맛집지도_준비/Node-Template-For-Food-Map-main/config/express.js
+++ 맛집지도_준비/Node-Template-For-Food-Map-main/config/express.js
@@ -6,19 +6,23 @@ var cors = require("cors");
 module.exports = function () {
   const app = express();
 
-  app.use(compression());
+  // 미들웨어 설정 : 미들웨어는 express 가 실행되면서 중간에 거쳐가는 것들을 의미
+  // 우리가 express 를 조금 더 사용하기 쉽게해주는 라이브러리들을 거쳐간다는 것
 
-  app.use(express.json());
+  app.use(compression()); // HTTP 요청을 압축 및 해제
 
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json()); // body 값을 파싱
 
-  app.use(methodOverride());
+  app.use(express.urlencoded({ extended: true })); // form 으로 제출되는 값 파싱
+ 
+  app.use(methodOverride()); // put, delete 요청 처리
 
-  app.use(cors());
+  app.use(cors()); // 웹브라우저 cors 설정을 관리
   app.use(express.static("/home/ubuntu/food-map/front"));
 
   // app.use(express.static(process.cwd() + '/public'));
 
+  // 직접 구현해야 하는 모듈
   require("../src/routes/indexRoute")(app);
 
   return app;

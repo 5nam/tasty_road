7932cabe547acb5ca132354d3bd0b3b436c22bc2
5nam <ohnam90@gmail.com>
2023-03-06 15:52:53 +0900

    style: 주석 추가

--- 맛집지도_준비/Node-Template-For-Food-Map-main/src/routes/indexRoute.js
+++ 맛집지도_준비/Node-Template-For-Food-Map-main/src/routes/indexRoute.js
@@ -3,6 +3,15 @@ module.exports = function (app) {
   const jwtMiddleware = require("../../config/jwtMiddleware");
 
   // 라우터 정의
+  // 어떤 요청이 들어오면 그것에 맞게 응답을 해주는 부분임.
+
   // app.HTTP메서드(uri, 컨트롤러 콜백함수)
   app.get("/dummy", index.example);
+
+  // 예시
+  /*
+  app.get("/dummy", function (req, res) { // req : 요청하는 객체, res : 응답하는 객체
+    res.send("get dummy 요청 성공");
+  })
+  */
 };

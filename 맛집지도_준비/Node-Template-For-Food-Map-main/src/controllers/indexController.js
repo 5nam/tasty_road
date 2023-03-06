2186b8e9e51fa53250d781bc6b92cc969393a838
5nam <ohnam90@gmail.com>
2023-03-06 16:48:58 +0900

    style: indexController 주석 추가

--- 맛집지도_준비/Node-Template-For-Food-Map-main/src/controllers/indexController.js
+++ 맛집지도_준비/Node-Template-For-Food-Map-main/src/controllers/indexController.js
@@ -1,4 +1,4 @@
-const { pool } = require("../../config/database");
+const { pool } = require("../../config/database"); // mySql 에 접근할 수 있는 객체
 const { logger } = require("../../config/winston");
 const jwt = require("jsonwebtoken");
 const secret = require("../../config/secret");
@@ -10,12 +10,25 @@ exports.example = async function (req, res) {
   try {
     const connection = await pool.getConnection(async (conn) => conn);
     try {
+      // mySql 접속과 관련된 부분을 정의하는 함수
+      // indexDao 부분에 데이터베이스 접근과 관련된 로직을 다 적어놓을 것
+      // 이 코드는 exampleDao 의 반환값이 배열의 첫 번째 요소를 rows 에 담겠다는 의미이다
       const [rows] = await indexDao.exampleDao(connection);
+      // 괄호 [] 는 es6 비구조 할당 문법임
+      /*
+      const a = 1;
+      const b = 2;
+      const c = 3;
+
+      const [a,b,c] = [1,2,3];
+      */
+
+      // rows 에다가 exampleDao 의 결과를 리턴, 그 리턴한 결과를 res 객체를 통해 send 해주는 것.
 
       return res.send({
         result: rows,
         isSuccess: true,
-        code: 200, // 요청 실패시 400번대 코드
+        code: 200, // 요청 실패시 400번대 코드, 요청 성공시 200번대 코드
         message: "요청 성공",
       });
     } catch (err) {
